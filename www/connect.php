07c91269a82c6a9978023ea6a27b1a950e2f1004
Ankur Arora <ankursmooth@gmail.com>
2015-10-19 03:56:19 +0530

    connect.php

---
+++ www/connect.php
@@ -0,0 +1,22 @@
+<?php
+
+ 
+define('MYSQL_USER', 'root');
+ 
+define('MYSQL_PASSWORD', '');
+ 
+define('MYSQL_HOST', 'localhost');
+
+define('MYSQL_DATABASE', 'sepmdatabase');
+$pdoOptions = array(
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_EMULATE_PREPARES => false
+);
+
+$pdo = new PDO(
+    "mysql:host=" . MYSQL_HOST . ";dbname=" . MYSQL_DATABASE, 
+    MYSQL_USER, 
+    MYSQL_PASSWORD, 
+    $pdoOptions 
+);
+
